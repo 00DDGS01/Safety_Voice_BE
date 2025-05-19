e02332ef51a794d1e4a904b497da58da910ee48f
newrhyme <ksh000624@naver.com>
2025-05-19 23:55:06 +0900

    [FEAT] : 회원가입 연동 및 서버 응답 메시지 처리

--- src/main/java/safety_voice/be/safety_voice_be/controller/AuthController.java
+++ src/main/java/safety_voice/be/safety_voice_be/controller/AuthController.java
@@ -2,12 +2,12 @@ package safety_voice.be.safety_voice_be.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import safety_voice.be.safety_voice_be.domain.dto.SignupRequest;
 import safety_voice.be.safety_voice_be.service.AuthService;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -16,9 +16,13 @@ public class AuthController {
 
     private final AuthService authService;
 
+    @CrossOrigin(origins = "*")
     @PostMapping("/signup")
-    public ResponseEntity<?> signup(@RequestBody SignupRequest request) {
+    public ResponseEntity<Map<String, String>> signup(@RequestBody SignupRequest request) {
         authService.signup(request);
-        return ResponseEntity.ok(" 회원가입이 완료되었습니다.");
+        return ResponseEntity
+                .ok()
+                .header("Content-Type", "application/json")
+                .body(Map.of("message", "회원가입이 완료되었습니다."));
     }
 }
