0c04586ffca7913f59beb7021fb12162cd401ab5
newrhyme <ksh000624@naver.com>
2025-05-20 01:14:13 +0900

    [FEAT] : 로그인 기능 구현 완료

--- src/main/java/safety_voice/be/safety_voice_be/controller/AuthController.java
+++ src/main/java/safety_voice/be/safety_voice_be/controller/AuthController.java
@@ -3,10 +3,11 @@ package safety_voice.be.safety_voice_be.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import safety_voice.be.safety_voice_be.domain.dto.SignupRequest;
+import safety_voice.be.safety_voice_be.domain.dto.LoginRequestDTO;
+import safety_voice.be.safety_voice_be.domain.dto.LoginResponseDTO;
+import safety_voice.be.safety_voice_be.domain.dto.SignupRequestDTO;
 import safety_voice.be.safety_voice_be.service.AuthService;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -18,11 +19,30 @@ public class AuthController {
 
     @CrossOrigin(origins = "*")
     @PostMapping("/signup")
-    public ResponseEntity<Map<String, String>> signup(@RequestBody SignupRequest request) {
+    public ResponseEntity<Map<String, String>> signup(@RequestBody SignupRequestDTO request) {
         authService.signup(request);
         return ResponseEntity
                 .ok()
                 .header("Content-Type", "application/json; charset=UTF-8")
                 .body(Map.of("message", "회원가입이 완료되었습니다."));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Map<String, String>> login(@RequestBody LoginRequestDTO request) {
+        try {
+            // 로그인 성공 시 토큰 포함된 응답 받기
+            LoginResponseDTO loginResponse = authService.login(request);
+            return ResponseEntity
+                    .ok()
+                    .header("Content-Type", "application/json; charset=UTF-8")
+                    .body(Map.of(
+                            "message", "로그인 성공",
+                            "token", loginResponse.getToken()
+                    ));
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity
+                    .status(401)
+                    .body(Map.of("message", e.getMessage()));
+        }
     }
 }
---
+++ src/main/java/safety_voice/be/safety_voice_be/domain/dto/LoginResponseDTO.java
@@ -0,0 +1,13 @@
+package safety_voice.be.safety_voice_be.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class LoginResponseDTO {
+    private String token;
+    private String message;
+}
